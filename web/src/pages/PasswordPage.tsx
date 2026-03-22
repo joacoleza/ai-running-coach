1b8b8447d2e27efbeed04037e1b3e5ce90e2f8b7
Joaquin <joacoleza@gmail.com>
2026-03-22 21:43:39 +0100

    revert: allow browser to remember password (remove autoComplete override)

--- web/src/pages/PasswordPage.tsx
+++ web/src/pages/PasswordPage.tsx
@@ -50,7 +50,6 @@ export function PasswordPage({ onSuccess }: PasswordPageProps) {
             placeholder="Enter password"
             className="w-full border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             disabled={loading}
-            autoComplete="new-password"
             autoFocus
           />
           <button
